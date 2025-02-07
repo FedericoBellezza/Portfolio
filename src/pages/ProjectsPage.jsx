bb8fd1e5003de330967c3993861d7d23571bcc1e
FedericoBellezza <federico.bellezza2001@gmail.com>
2025-02-07 17:14:10 +0100

    add weatherapp in projects

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -2,82 +2,46 @@ export default function ProjectsPage() {
   return (
     <div className="bg-slate-900  text-amber-50 pt-25 min-h-screen">
       <div className="container p-10 mx-auto py-10">
-        {/* 1st row */}
+        {/* title */}
         <h1 className="lg:text-7xl text-5xl text-center font-extrabold animate-entryfromright ">
           I miei progetti
         </h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10  animate-entryfromleft mt-15 ">
-          <div className="project-card  bg-emerald-400 w-full h-70 overflow-hidden text-white font-bold py-2  rounded-xl relative duration-200 ease  ">
-            <p className="text-4xl lg:text-6xl p-5 text-end">Progetto 1</p>
-            <i className="fa-brands fa-github text-[20rem] top-0 left-0 absolute opacity-30"></i>
-
-            <button className="absolute bottom-10 right-10 cursor-pointer">
-              <i className="fa-solid fa-arrow-right text-2xl p-2 border  rounded hover:bg-amber-50 hover:text-slate-600 hover:border-transparent duration-200 ease "></i>
-            </button>
+        {/* card container */}
+        <div className=" grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10  animate-entryfromleft mt-15">
+          {/* weather app */}
+          <div
+            onClick={() =>
+              window.open("https://weatherapp-federicobellezza.web.app/")
+            }
+            className="cursor-pointer project-card  bg-sky-400 w-full h-70 overflow-hidden text-white font-bold py-2 text-4xl xl:text-5xl hover:scale-105   rounded-xl relative duration-200 ease  "
+          >
+            <p className="p-5 px-10 text-end">WeatherApp</p>
+            <i className="fa-solid fa-cloud text-[20rem] top-0 left-0 absolute opacity-30"></i>
           </div>
-          <div className="project-card  bg-purple-400 w-full h-70 overflow-hidden   text-white font-bold py-2  rounded-xl relative duration-200 ease ">
-            <p className="text-4xl lg:text-6xl p-5 text-end">Progetto 2</p>
-            <i className="fa-brands fa-github text-[20rem] top-0 left-0 absolute opacity-30"></i>
-
-            <button className="absolute bottom-10 right-10 cursor-pointer">
-              <i className="fa-solid fa-arrow-right text-2xl p-2 border  rounded hover:bg-amber-50 hover:text-slate-600 hover:border-transparent duration-200 ease "></i>
-            </button>
+          {/* In corso */}
+          <div className="opacity-50 project-card  bg-red-400 w-full h-70 overflow-hidden text-white font-bold py-2 text-4xl xl:text-5xl    rounded-xl relative duration-200 ease  ">
+            <p className="p-5  px-10  text-end ">In corso</p>
+            <i className="fa-solid fa-cloud text-[20rem] top-0 left-0 absolute opacity-30"></i>
           </div>
-          <div className="project-card  bg-sky-400 w-full h-70 overflow-hidden   text-white font-bold py-2  rounded-xl relative duration-200 ease ">
-            <p className="text-4xl lg:text-6xl p-5 text-end">Progetto 3</p>
-            <i className="fa-brands fa-github text-[20rem] top-0 left-0 absolute opacity-30"></i>
-
-            <button className="absolute bottom-10 right-10 cursor-pointer">
-              <i className="fa-solid fa-arrow-right text-2xl p-2 border  rounded hover:bg-amber-50 hover:text-slate-600 hover:border-transparent duration-200 ease "></i>
-            </button>
+          {/* In corso */}
+          <div className="opacity-50 project-card  bg-yellow-400 w-full h-70 overflow-hidden text-white font-bold py-2 text-4xl xl:text-5xl    rounded-xl relative duration-200 ease  ">
+            <p className="p-5  px-10  text-end ">In corso</p>
+            <i className="fa-solid fa-cloud text-[20rem] top-0 left-0 absolute opacity-30"></i>
           </div>
-          <div className="project-card  bg-pink-400 w-full h-70 overflow-hidden text-white font-bold py-2  rounded-xl relative duration-200 ease  ">
-            <p className="text-4xl lg:text-6xl p-5 text-end">Progetto 4</p>
-            <i className="fa-brands fa-github text-[20rem] top-0 left-0 absolute opacity-30"></i>
-
-            <button className="absolute bottom-10 right-10 cursor-pointer">
-              <i className="fa-solid fa-arrow-right text-2xl p-2 border  rounded hover:bg-amber-50 hover:text-slate-600 hover:border-transparent duration-200 ease "></i>
-            </button>
-          </div>{" "}
-          <div className="project-card  bg-green-400 w-full h-70 overflow-hidden text-white font-bold py-2  rounded-xl relative duration-200 ease  ">
-            <p className="text-4xl lg:text-6xl p-5 text-end">Progetto 5</p>
-            <i className="fa-brands fa-github text-[20rem] top-0 left-0 absolute opacity-30"></i>
-
-            <button className="absolute bottom-10 right-10 cursor-pointer">
-              <i className="fa-solid fa-arrow-right text-2xl p-2 border  rounded hover:bg-amber-50 hover:text-slate-600 hover:border-transparent duration-200 ease "></i>
-            </button>
-          </div>{" "}
-          <div className="project-card  bg-fuchsia-400 w-full h-70 overflow-hidden text-white font-bold py-2  rounded-xl relative duration-200 ease  ">
-            <p className="text-4xl lg:text-6xl p-5 text-end">Progetto 6</p>
-            <i className="fa-brands fa-github text-[20rem] top-0 left-0 absolute opacity-30"></i>
-
-            <button className="absolute bottom-10 right-10 cursor-pointer">
-              <i className="fa-solid fa-arrow-right text-2xl p-2 border  rounded hover:bg-amber-50 hover:text-slate-600 hover:border-transparent duration-200 ease "></i>
-            </button>
-          </div>{" "}
-          <div className="project-card  bg-lime-400 w-full h-70 overflow-hidden text-white font-bold py-2  rounded-xl relative duration-200 ease  ">
-            <p className="text-4xl lg:text-6xl p-5 text-end">Progetto 7</p>
-            <i className="fa-brands fa-github text-[20rem] top-0 left-0 absolute opacity-30"></i>
-
-            <button className="absolute bottom-10 right-10 cursor-pointer">
-              <i className="fa-solid fa-arrow-right text-2xl p-2 border  rounded hover:bg-amber-50 hover:text-slate-600 hover:border-transparent duration-200 ease "></i>
-            </button>
-          </div>{" "}
-          <div className="project-card  bg-yellow-400 w-full h-70 overflow-hidden text-white font-bold py-2  rounded-xl relative duration-200 ease  ">
-            <p className="text-4xl lg:text-6xl p-5 text-end">Progetto 8</p>
-            <i className="fa-brands fa-github text-[20rem] top-0 left-0 absolute opacity-30"></i>
-
-            <button className="absolute bottom-10 right-10 cursor-pointer">
-              <i className="fa-solid fa-arrow-right text-2xl p-2 border  rounded hover:bg-amber-50 hover:text-slate-600 hover:border-transparent duration-200 ease "></i>
-            </button>
-          </div>{" "}
-          <div className="project-card  bg-red-400 w-full h-70 overflow-hidden text-white font-bold py-2  rounded-xl relative duration-200 ease  ">
-            <p className="text-4xl lg:text-6xl p-5 text-end">Progetto 9</p>
-            <i className="fa-brands fa-github text-[20rem] top-0 left-0 absolute opacity-30"></i>
-
-            <button className="absolute bottom-10 right-10 cursor-pointer">
-              <i className="fa-solid fa-arrow-right text-2xl p-2 border  rounded hover:bg-amber-50 hover:text-slate-600 hover:border-transparent duration-200 ease "></i>
-            </button>
+          {/* In corso */}
+          <div className="opacity-50 project-card  bg-green-400 w-full h-70 overflow-hidden text-white font-bold py-2 text-4xl xl:text-5xl    rounded-xl relative duration-200 ease  ">
+            <p className="p-5  px-10  text-end ">In corso</p>
+            <i className="fa-solid fa-cloud text-[20rem] top-0 left-0 absolute opacity-30"></i>
+          </div>
+          {/* In corso */}
+          <div className="opacity-50 project-card  bg-pink-400 w-full h-70 overflow-hidden text-white font-bold py-2 text-4xl xl:text-5xl    rounded-xl relative duration-200 ease  ">
+            <p className="p-5  px-10  text-end ">In corso</p>
+            <i className="fa-solid fa-cloud text-[20rem] top-0 left-0 absolute opacity-30"></i>
+          </div>
+          {/* In corso */}
+          <div className="opacity-50 project-card  bg-orange-400 w-full h-70 overflow-hidden text-white font-bold py-2 text-4xl xl:text-5xl    rounded-xl relative duration-200 ease  ">
+            <p className="p-5  px-10  text-end ">In corso</p>
+            <i className="fa-solid fa-cloud text-[20rem] top-0 left-0 absolute opacity-30"></i>
           </div>
         </div>
       </div>
