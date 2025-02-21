494c526574e0c811e7e51faada0742ed35bd5cfa
FedericoBellezza <federico.bellezza2001@gmail.com>
2025-02-21 19:25:40 +0100

    add flaver project

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -34,67 +34,75 @@ export default function ContactsPage() {
     }, 3000);
   };
   return (
-    <div className="bg-slate-900  text-amber-50 pt-25 min-h-screen">
-      <div className="container px-10   mx-auto flex flex-col xl:flex-row mt-25  justify-evenly items-center xl:items-start">
-        <h1 className="lg:text-7xl text-5xl text-center font-extrabold animate-entryfromleft mb-10">
-          Contattami
-          <span
-            className={`text-center xl:text-left mt-5  text-2xl block duration-500  ease-in-out    ${
-              emailSent ? "opacity-100 animate-entryfromright" : "opacity-0 "
-            }`}
-          >
-            Email Inviata con successo!
-          </span>
-        </h1>
-        <div className="animate-entryfromright  max-w-xl">
-          <form
-            onSubmit={sendEmail}
-            className={` space-y-4 w-full duration-500 ease-in-out lg:relative lg:top-0  ${
-              emailSent ? "relative top-0" : "top-[-80px] relative"
-            } `}
-          >
-            <input
-              required
-              type="text"
-              name="from_name"
-              placeholder="Nome"
-              className="w-full py-2.5 px-4 text-gray-800 bg-gray-100 border rounded-lg  text-sm outline-none transition-all"
-            />
-            <input
-              required
-              type="email"
-              name="from_email"
-              placeholder="Email"
-              className="w-full py-2.5 px-4 text-gray-800 bg-gray-100 border rounded-lg text-sm outline-none transition-all"
-            />
-            <input
-              required
-              type="text"
-              placeholder="Oggetto"
-              name="object"
-              className="w-full py-2.5 px-4 text-gray-800 bg-gray-100 border rounded-lg text-sm outline-none transition-all"
-            />
-            <textarea
-              required
-              placeholder="Messaggio"
-              rows="4"
-              name="message"
-              className="w-full px-4 text-gray-800 bg-gray-100 border rounded-lg text-sm pt-3 outline-none transition-all"
-            ></textarea>
+    <>
+      <img
+        src="./11.svg"
+        alt=""
+        className="absolute w-55/100 xl:w-45/100  xl:top-[-50px] left-20 hidden xl:block animate-entryfromleft"
+      />
 
-            <input
-              name="submitbutton"
-              className="text-white font-bold cursor-pointer bg-slate-700 hover:bg-lime-300 hover:text-slate-800 rounded-lg tracking-wide text-sm px-4 py-2.5 w-full transition-all ease-in duration-100"
-              type="submit"
-              value="Invia"
-            />
-          </form>
+      <div className="bg-slate-900  text-amber-50 pt-25 min-h-screen">
+        <div className="container px-10   mx-auto flex flex-col xl:flex-row mt-25  justify-evenly items-center xl:items-start relative">
+          <h1 className="lg:text-7xl text-5xl text-center font-extrabold animate-entryfromleft mb-10">
+            Contattami
+            <span
+              className={`text-center xl:text-left mt-5  text-2xl block duration-500  ease-in-out    ${
+                emailSent ? "opacity-100 animate-entryfromright" : "opacity-0 "
+              }`}
+            >
+              Email Inviata con successo!
+            </span>
+          </h1>
+          <div className="animate-entryfromright  max-w-xl">
+            <form
+              onSubmit={sendEmail}
+              className={` space-y-4 w-full duration-500 ease-in-out lg:relative lg:top-0  ${
+                emailSent ? "relative top-0" : "top-[-80px] relative"
+              } `}
+            >
+              <input
+                required
+                type="text"
+                name="from_name"
+                placeholder="Nome"
+                className="w-full py-2.5 px-4 text-gray-800 bg-gray-100 border rounded-lg  text-sm outline-none transition-all"
+              />
+              <input
+                required
+                type="email"
+                name="from_email"
+                placeholder="Email"
+                className="w-full py-2.5 px-4 text-gray-800 bg-gray-100 border rounded-lg text-sm outline-none transition-all"
+              />
+              <input
+                required
+                type="text"
+                placeholder="Oggetto"
+                name="object"
+                className="w-full py-2.5 px-4 text-gray-800 bg-gray-100 border rounded-lg text-sm outline-none transition-all"
+              />
+              <textarea
+                required
+                placeholder="Messaggio"
+                rows="4"
+                name="message"
+                className="w-full px-4 text-gray-800 bg-gray-100 border rounded-lg text-sm pt-3 outline-none transition-all"
+              ></textarea>
+
+              <input
+                name="submitbutton"
+                className="text-white font-bold cursor-pointer bg-slate-700 hover:bg-lime-300 hover:text-slate-800 rounded-lg tracking-wide text-sm px-4 py-2.5 w-full transition-all ease-in duration-100"
+                type="submit"
+                value="Invia"
+              />
+            </form>
+          </div>
         </div>
+        <p className="text-lg text-gray-400 lg:mt-50 mt-15  text-center">
+          Oppure scrivimi alla email:{" "}
+          <strong>federico.bellezza2001@gmail.com</strong>
+        </p>
       </div>
-      <p className="text-lg text-gray-400 lg:mt-50 mt-15  text-center">
-        Oppure scrivimi alla email:{" "}
-        <strong>federico.bellezza2001@gmail.com</strong>
-      </p>
-    </div>
+    </>
   );
 }
--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -4,7 +4,7 @@ export default function ProjectsPage() {
       <div className="container p-10 mx-auto py-10">
         {/* title */}
         <h1 className="lg:text-7xl text-5xl text-center font-extrabold animate-entryfromright ">
-          I miei progetti
+          Progetti personali
         </h1>
         {/* card container */}
         <div className=" grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10  animate-entryfromleft mt-15">
@@ -16,32 +16,21 @@ export default function ProjectsPage() {
             className="cursor-pointer project-card  bg-sky-400 w-full h-70 overflow-hidden text-white font-bold py-2 text-4xl xl:text-5xl hover:scale-105   rounded-xl relative duration-200 ease  "
           >
             <p className="p-5 px-10 text-end">WeatherApp</p>
+            <p className="  px-10  text-lg text-end ">In corso</p>
+
             <i className="fa-solid fa-cloud text-[20rem] top-0 left-0 absolute opacity-30"></i>
           </div>
           {/* In corso */}
-          <div className="opacity-50 project-card  bg-red-400 w-full h-70 overflow-hidden text-white font-bold py-2 text-4xl xl:text-5xl    rounded-xl relative duration-200 ease  ">
-            <p className="p-5  px-10  text-end ">In corso</p>
-            <i className="fa-solid fa-cloud text-[20rem] top-0 left-0 absolute opacity-30"></i>
-          </div>
-          {/* In corso */}
-          <div className="opacity-50 project-card  bg-yellow-400 w-full h-70 overflow-hidden text-white font-bold py-2 text-4xl xl:text-5xl    rounded-xl relative duration-200 ease  ">
-            <p className="p-5  px-10  text-end ">In corso</p>
-            <i className="fa-solid fa-cloud text-[20rem] top-0 left-0 absolute opacity-30"></i>
-          </div>
-          {/* In corso */}
-          <div className="opacity-50 project-card  bg-green-400 w-full h-70 overflow-hidden text-white font-bold py-2 text-4xl xl:text-5xl    rounded-xl relative duration-200 ease  ">
-            <p className="p-5  px-10  text-end ">In corso</p>
-            <i className="fa-solid fa-cloud text-[20rem] top-0 left-0 absolute opacity-30"></i>
-          </div>
-          {/* In corso */}
-          <div className="opacity-50 project-card  bg-pink-400 w-full h-70 overflow-hidden text-white font-bold py-2 text-4xl xl:text-5xl    rounded-xl relative duration-200 ease  ">
-            <p className="p-5  px-10  text-end ">In corso</p>
-            <i className="fa-solid fa-cloud text-[20rem] top-0 left-0 absolute opacity-30"></i>
-          </div>
-          {/* In corso */}
-          <div className="opacity-50 project-card  bg-orange-400 w-full h-70 overflow-hidden text-white font-bold py-2 text-4xl xl:text-5xl    rounded-xl relative duration-200 ease  ">
-            <p className="p-5  px-10  text-end ">In corso</p>
-            <i className="fa-solid fa-cloud text-[20rem] top-0 left-0 absolute opacity-30"></i>
+          <div
+            onClick={() =>
+              window.open("https://flaverprotection-b057e.web.app/")
+            }
+            className="cursor-pointer project-card  bg-green-400 w-full h-70 overflow-hidden text-white font-bold py-2 text-4xl xl:text-5xl  hover:scale-105  rounded-xl relative duration-200 ease  "
+          >
+            <p className="p-5  px-10  text-end ">Flaver Protection</p>
+            <p className="  px-10  text-lg text-end ">In corso</p>
+
+            <i className="fa-solid fa-tools text-[20rem] top-0 left-0 absolute opacity-30"></i>
           </div>
         </div>
       </div>
